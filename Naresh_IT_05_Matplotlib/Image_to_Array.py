2f1d8760e96741c74706a27ae40e462c56e49a43
Nikhil Mahapatro <nikhilmahapatro2002@gmail.com>
2025-06-04 12:37:48 +0530

    Add files via upload

---
+++ Naresh_IT_05_Matplotlib/Image_to_Array.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image                  # PIL- Python Imaging Library
+
+ones_arr = np.ones((5,5), dtype= int)
+print(ones_arr)
+
+# every pixel range is 1-255
+print(ones_arr * 255)
+
+cap_image = Image.open(r"C:\Users\nikhi\PycharmProjects\NARESH_IT\01_NARESH_IT_PYTHON\Naresh_IT_05_Matplotlib\peakpx.jpg")
+# cap_image.show()
+print(type(cap_image))
+
+cap_arr = np.asarray(cap_image)
+print(cap_arr)
+print(type(cap_arr))
+
+plt.imshow(cap_arr)
+#plt.show()
+print(cap_arr.shape)
+
+cap_red = cap_arr.copy()
+print(cap_red)
+print(cap_arr==cap_red)
+
+# Changing colour of the array.
+# R-G-B
+# 0=red, 1=blue, 2=green
+plt.imshow(cap_red[:,:,0])
+#plt.show()
+plt.imshow(cap_red[:,:,0],cmap='Greys')
+#plt.show()
+plt.imshow(cap_red[:,:,1],cmap='grey')
+#plt.show()
+plt.imshow(cap_red[:,:,1],cmap='Blues')
+#plt.show()
+plt.imshow(cap_red[:,:,2],cmap='YlGn')
+plt.show()
+
+
+
+
+
