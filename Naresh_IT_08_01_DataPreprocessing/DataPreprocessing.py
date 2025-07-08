860ca3d94c0621beaef6ee833d54ac1c479a86b2
Nikhil Mahapatro <nikhilmahapatro2002@gmail.com>
2025-07-08 13:18:19 +0530

    Update and rename ML_practical.py to DataPreprocessing.py

--- Naresh_IT_08_01_DataPreprocessing/DataPreprocessing.py
+++ Naresh_IT_08_01_DataPreprocessing/DataPreprocessing.py
@@ -38,11 +38,6 @@ Y = LabelEncoder_Y.fit_transform(Y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2, random_state=0)
 
-#_____________________________________________________________________________
-
-# REGRESSION MODELS
-
-# SIMPLE LINEAR REGRESSION
 
 
 
