b9f0af2243db54457f90d2272308f459ebe8a225
Nikhil Mahapatro <nikhilmahapatro2002@gmail.com>
2025-07-24 19:17:50 +0530

    Add files via upload

--- Naresh_IT_08_10_LogisticRegression/Logistic_Regression.py
+++ Naresh_IT_08_10_LogisticRegression/Logistic_Regression.py
@@ -45,12 +45,59 @@ print(variance)
 # acc= 92.5, bias=82, var= 92.5..................best fit model
 
 
+# FUTURE PREDICTIONS --------------------------
 
+dataset1 = pd.read_csv(r"C:\Users\nikhi\PycharmProjects\NARESH_IT\08_NARESH_IT_MACHINE_LEARNING\Naresh_IT_08_10_LogisticRegression\Future prediction1.csv")
 
+d2 = dataset1.copy()
 
+dataset1 = dataset1.iloc[:, [2, 3]].values
 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+M = sc.fit_transform(dataset1)
 
+y_pred1 = pd.DataFrame()
 
+d2 ['y_pred1'] = classifier.predict(M)
+
+d2.to_csv('final1.csv')
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('blue', 'yellow'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('blue', 'yellow'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
 
 
 
