c0d97f359fc42af3d219000bdc26f24e35ce40c2
Nikhil Mahapatro <nikhilmahapatro2002@gmail.com>
2025-08-12 02:05:57 +0530

    Add files via upload

---
+++ Naresh_IT_08_21_HierarchialClustering/Hierarchial_Clustering.py
@@ -0,0 +1,39 @@
+#----------------------------CLUSTERING MODELS---------------------------#
+
+# HIERARCHIAL CLUSTERING
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r"C:\Users\nikhi\PycharmProjects\NARESH_IT\08_NARESH_IT_MACHINE_LEARNING\Naresh_IT_08_21_HierarchialClustering\Mall_Customers.csv")
+
+X = dataset.iloc[:, [3, 4]].values
+
+import scipy.cluster.hierarchy as sch
+
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+# Training the Hierarchical Clustering model on the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5,  linkage = 'ward')
+y_hc = hc.fit_predict(X)
+
+# Visualising the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+dataset['cluster'] =y_hc
